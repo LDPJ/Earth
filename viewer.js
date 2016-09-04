b70301e3fa5731cc4fdfa317347f763096d3796e
Nah.Myself( ) <larrypaulingjr@gmail.com>
2016-09-04 17:35:25 -0400

    Create viewer.js

---
+++ viewer.js
@@ -0,0 +1,22 @@
+var request;
+var $current;
+var cache = {};
+var $frame = $('#photo-viewer');
+var $thumbs = $('.thumb');
+
+function crossfade($img) {
+
+if ($current) {
+$current.stop().fadeOut('slow');
+}
+
+$img.css({
+marginLeft: -$img.width() / 2,
+marginTop: -$img.height() / 2
+});
+
+$img.stop().fadeTo('slow',1);
+
+$current =$img;
+
+}
