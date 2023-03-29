75d4485814830112ad934d7912114f38e9041fae
LDPJ <larrypaulingjr@gmail.com>
2023-03-29 09:04:55 -0400

    Update APIMEDIASTACK.js

--- APIMEDIASTACK.js
+++ APIMEDIASTACK.js
@@ -1,5 +1,12 @@
+async function getUsers() {
+    let response = await fetch ('https://api.mediastack.com/v1/sources
+                                                                        ? access_key = ff5ee090da45cca55145606c580060c2
+                                                                        & search = abc
+                                ');
+    let data = await response.json()
+    return data;
+}
 
-http://api.mediastack.com/v1/sources
-    ? access_key = ff5ee090da45cca55145606c580060c2
-    & search = abc
+getUsers().then(data console.log(Data))';
+    
 
